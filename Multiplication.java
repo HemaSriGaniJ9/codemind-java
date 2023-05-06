52f44c25c6673cc8d13b3bde32051c98cee72827
HemaSriGaniJ9 <119720904+HemaSriGaniJ9@users.noreply.github.com>
2023-05-06 11:22:34 +0530

    import java.util.Scanner;
     public class MyClass
     {
      public static void main(String[] args)
      {
        int x, y, mul;
        Scanner sc = new Scanner(System.in); 
        x = sc.nextInt();
        y = sc.nextInt(); 
        mul = x * y; 
        System.out.println(mul); 
      }
    }

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+
+ public class MyClass
+ {
+  public static void main(String[] args)
+  {
+    int x, y, mul;
+    Scanner sc = new Scanner(System.in); 
+    x = sc.nextInt();
+    y = sc.nextInt(); 
+    mul = x * y; 
+    System.out.println(mul); 
+  }
+}
