55c0c678753d4977e6553642da4c4666fa24ce29
HemaSriGaniJ9 <119720904+HemaSriGaniJ9@users.noreply.github.com>
2023-05-12 21:22:56 +0530

    import java.util.*; public class FindMaximumNumberOfHanddhakes {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int n,x;         n=sc.nextInt();         x=((n-1)*n)/2;         System.out.println(x);
        }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FindMaximumNumberOfHanddhakes
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n,x;
+        n=sc.nextInt();
+        x=((n-1)*n)/2;
+        System.out.println(x);
+        
+    }
+}
